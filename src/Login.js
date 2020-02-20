98809ceb8862aaffa69a18338fcdbf8d96df1bdf
Daniel Rodriguez <daniel.rodriguez-vi@mail.escuelaing.edu.co>
2020-02-20 17:01:34 -0500

    SignUp finish

--- src/Login.js
+++ src/Login.js
@@ -18,7 +18,7 @@ function Copyright() {
         <Typography variant="body2" color="textSecondary" align="center">
             {'Copyright © '}
             <Link color="inherit" href="https://material-ui.com/">
-                Your Website
+                Escuela Colombiana de Ingenieria Julio Garavito
             </Link>{' '}
             {new Date().getFullYear()}
             {'.'}
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import VistaCal from './vistaCal';
+import SignUp from "./signUp";
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
